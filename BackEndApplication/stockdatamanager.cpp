f63b5de4bcb86f418f44da0f3b79d03528d64ac4
christopher pelech <christopherglynnpelech@gmail.com>
2016-05-18 10:11:13 -0500

    Search update

--- BackEndApplication/stockdatamanager.cpp
+++ BackEndApplication/stockdatamanager.cpp
@@ -9,9 +9,31 @@ StockDataManager::StockDataManager()
     readFile("companylist3.csv",stockList);
 }
 
-std::vector<StockListing*> StockDataManager::search(std::string symbol )
+std::vector<StockListing *> *StockDataManager::SymbolSearch(std::string symbol )
 {
+    std::cout<<"searach"<<std::endl;
 
+    if(cache.KeyHas(symbol))
+    {
+        std::cout<<"cache hit"<<std::endl;
+        return cache.EntryGet(symbol);
+    }
+    else
+    {
+       std::vector<StockListing *>* temp = new std::vector<StockListing*>();
+        std::cout<<"cache miss"<<symbol<<std::endl;
+
+       for(int i = 0; i < stockList.size(); i++)
+       {
+           if(stockList[i]->name.find(symbol) != std::string::npos)
+           {
+               std::cout<<"match"<<std::endl;
+               temp->push_back(stockList[i]);
+           }
+       }
+       cache.UpdateEntry(symbol,temp);
+       return temp;
+    }
 }
 
 std::string StockDataManager::StockListJsonGet(std::string search)
@@ -23,15 +45,29 @@ std::string StockDataManager::StockListJsonGet(std::string search)
     jw.OverideNextCommaRule();
     jw.add("id", search);
 
-    std::vector<StockListing*>* searchResult = search(symbol);
+    std::vector<StockListing*>* searchResult = SymbolSearch(search);
 
+    jw.writeCommaAsNeeded();
+    jw.writeNameLabel("listofstocks");
 
+    jw.BracketOpen();
+    for(int i = 0; i < searchResult->size(); i++)
+    {
+        std::cout<<"as"<<std::endl;
+        jw.addHelper(searchResult->at(i)->myId);
+        if(i + 1 != searchResult->size())
+        {
+            jw.writeCommaAsNeeded();
+        }
 
+    }
+    jw.BracketClose();
 
-    jw.writeIDArray("listofstocks", searchResult.size());
     jw.objClose();
 
-    jw.addArrayPointer("stocks",searchResult);
+    jw.addArrayPointer("stocks",*searchResult);
+
+
     std::cout<<"done"<<std::endl;
     return jw.jsonStringGet();
 }
